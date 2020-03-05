53ee01a84bfef3b50e3aab55766e0539c7997f93
Felipe D. santos <lycan.felipe@gmail.com>
2020-03-04 22:39:27 -0300

    Created forms login and register and add lib bcryptjs

---
+++ LoginSystem/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcryptjs');
+
+const users = [];
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: 'Felipe D. Santos'});
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+    
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect('/login');
+
+    }catch{
+        res.redirect('/register');
+    }
+    console.log('Register sucedfull: ' + users);
+});
+
+
+
+app.listen(8080);
+
