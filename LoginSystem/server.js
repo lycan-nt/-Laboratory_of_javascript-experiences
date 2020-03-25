21f1f1e26f53ceb9ec315fcc18dc752ef34abdcb
Felipe D. santos <lycan.felipe@gmail.com>
2020-03-25 19:56:34 -0300

    .env

--- LoginSystem/server.js
+++ LoginSystem/server.js
@@ -1,9 +1,13 @@
+if(process.env.NODE_ENV !== 'production') {
+    require(dotenv).config();
+}
+
 const express = require('express');
 const app = express();
 const bcryptjs = require('bcryptjs');
 const passport = require('passport');
 const flash = require('express-flash');
-const session = require('express-sesseion');
+const session = require('express-session');
 
 const initializePassport = require('./passport-config');
 initializePassport(passport, email => {
@@ -16,8 +20,14 @@ app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(session({
-    
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'Felipe D. Santos'});
@@ -27,9 +37,12 @@ app.get('/login', (req, res) => {
     res.render('login.ejs');
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
     
-});
+}));
 
 app.get('/register', (req, res) => {
     res.render('register.ejs');
