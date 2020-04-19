aa84c984d95320ae798b7beaa43b2376c09de38d
Felipe D. santos <lycan.felipe@gmail.com>
2020-04-19 18:29:30 -0300

    Criat a route post and insert lib bcrypt

--- Estrutura_De_Cadastro/src/controllers/authcontroller.js
+++ Estrutura_De_Cadastro/src/controllers/authcontroller.js
@@ -1,6 +1,25 @@
-const express = require(express);
+const express = require('express');
 
 const User = require("../models/user");
 
 const router = express.Router();
 
+router.post('/register', async (req, res) => {
+  const { email } = req.body;  
+
+  try {
+
+    if(await User.findOne({ email }))
+        return res.status(400).send({ error: "User already existy" });
+
+      const user = await User.create(req.body);
+
+      user.password = undefined;
+
+      return res.send({ user });
+  } catch(err) {
+      return res.status(400).send({ error: "Registraition failed" });
+  }
+});
+
+module.exports = app => app.use("/auth", router);
