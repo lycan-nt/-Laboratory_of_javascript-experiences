9673b00fa8a8084a58f453670beea70cac77cb7d
Felipe D. santos <lycan.felipe@gmail.com>
2020-04-20 12:57:36 -0300

    bcrypt compare password

--- Estrutura_De_Cadastro/src/controllers/authcontroller.js
+++ Estrutura_De_Cadastro/src/controllers/authcontroller.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -20,6 +21,20 @@ router.post('/register', async (req, res) => {
   } catch(err) {
       return res.status(400).send({ error: "Registraition failed" });
   }
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).selected("+password");
+
+  if(!user)
+    return res.status(400).send({ error: "User not found" });
+
+  if(!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: "Invalid password" });
+  
+  res.send({ user });
 });
 
 module.exports = app => app.use("/auth", router);
