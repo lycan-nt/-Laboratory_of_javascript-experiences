1f6fda80dffca13f76c15a9a849968e29dde772b
Felipe D. santos <lycan.felipe@gmail.com>
2020-04-20 13:19:25 -0300

    Autchenticate password

--- Estrutura_De_Cadastro/src/controllers/authcontroller.js
+++ Estrutura_De_Cadastro/src/controllers/authcontroller.js
@@ -26,7 +26,7 @@ router.post('/register', async (req, res) => {
 router.post("/authenticate", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email }).selected("+password");
+  const user = await User.findOne({ email }).select("+password");
 
   if(!user)
     return res.status(400).send({ error: "User not found" });
