bf19080fef73f5d5bcf9609a31e85ffebaec0b80
Felipe D. Santos <lycan.felipe@gmail.com>
2020-06-21 02:00:34 -0300

    Simple Kanban Board

---
+++ simple-kanban-board/script.js
@@ -0,0 +1,56 @@
+const cards = document.querySelectorAll(".card");
+const dropzones = document.querySelectorAll(".dropzone");
+
+/** Our cards **/
+cards.forEach( card => {
+    card.addEventListener("dragstart", dragstart);
+    card.addEventListener("drag", drag);
+    card.addEventListener("dragend", dragend);
+});
+
+function dragstart() {
+    dropzones.forEach( dropzone => dropzone.classList.add("highlight"));
+
+    this.classList.add("is-dragging");
+}
+
+function drag() {
+
+}
+
+function dragend() {
+    dropzones.forEach( dropzone => dropzone.classList.remove("highlight"));
+
+    this.classList.remove("is-dragging");
+
+}
+
+/** Place where we will drop cards */
+dropzones.forEach( dropzone => {
+    dropzone.addEventListener("dragenter", dragenter);
+    dropzone.addEventListener("dragover", dragover);
+    dropzone.addEventListener("dragleave", dragleave);
+    dropzone.addEventListener("drog", drop);
+});
+
+function dragenter() {
+
+}
+
+function dragover() {
+    this.classList.add("over");
+
+    const cardBeingDragged = document.querySelector(".is-dragging");
+
+    this.appendChild(cardBeingDragged);
+}
+
+function dragleave() {
+    this.classList.remove("over");
+
+}
+
+function drop() {
+   // this.classList.remove("over");
+    
+}
